4d985750655231e5d6c726b35eb0af9b598a724f
Fredrik <flarsson93@gmail.com>
2016-04-19 13:21:24 +0200

    more readable

--- src/places/Position.java
+++ src/places/Position.java
@@ -22,8 +22,8 @@ public class Position {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + x;
-		result = prime * result + y;
+		result = result * prime + x;
+		result = result * prime + y;
 		return result;
 	}
 
